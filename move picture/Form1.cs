8244a9dc1ea4127717f37124ea6b93a98650c380
Kieran James Challis <rc0033607@student.riversidecollege.ac.uk>
2018-11-20 10:32:51 +0000

    changed the number 5 to 10

--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -19,7 +19,7 @@ namespace move_picture
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
         }
     }
 }
